22bb594b6ad03768863d43881bf9de8e46522abb
qwe11qwe <rlatjdwo811@gmail.com>
2023-07-16 23:42:24 +0900

    naver_login

---
+++ mission-01/naver_login/js/main.js
@@ -0,0 +1,42 @@
+const user = {
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
+
+/*
+
+1. email 정규표현식을 사용한 validation
+2. pw 정규표현식을 사용한 validation
+3. 상태 변수 관리
+4. 로그인 버튼을 클릭시 조건처리
+
+*/
+
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase());
+}
+
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const usernameInput = document.getElementById("id");
+  const passwordInput = document.getElementById("pw");
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (user) {
+    const welcomePageURL = "welcome.html";
+  }
+}
